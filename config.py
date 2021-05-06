da21042069b87e87256cc98a8a7f932a22b18a6d
Julio <jn5.guape@gmail.com>
2021-05-06 19:33:30 -0300

    config-class

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/satelp03_db_portal'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY = 'um-nome-bem-seguro'
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))  # Diretorio do projeto
+
+class Config(object):
+    DEBUG = True
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "um-nome-bem-seguro"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "database.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PAGINATION = 10
